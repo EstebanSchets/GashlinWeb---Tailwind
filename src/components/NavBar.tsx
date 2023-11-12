9fc9e443db908acdfa32c894e1aff481c9c2f39b
EstebanSchets <EstebanSchets>
2023-11-12 18:16:48 +0000

    Wide Screen Addition

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -32,21 +32,21 @@ const NavBar: FunctionComponent = () => {
     };
   }, []);
   return (
-    <div className="home-parent">
-      <button className="home1" data-animate-on-scroll>
+    <div className="home-group">
+      <button className="home2" data-animate-on-scroll>
         Home
       </button>
-      <button className="music1" data-animate-on-scroll>
+      <button className="music2" data-animate-on-scroll>
         Music
       </button>
-      <button className="login1" data-animate-on-scroll>
+      <button className="login2" data-animate-on-scroll>
         Login
       </button>
-      <button className="qa1" data-animate-on-scroll>{`Q&A`}</button>
-      <button className="about1" data-animate-on-scroll>
+      <button className="qa2" data-animate-on-scroll>{`Q&A`}</button>
+      <button className="about2" data-animate-on-scroll>
         About
       </button>
-      <button className="merch1" data-animate-on-scroll>
+      <button className="merch2" data-animate-on-scroll>
         Merch
       </button>
     </div>
